9f4f39af9728bbbec70edab8228d16c92672b628
Cherobin <ricardo@cherobin.com.br>
2020-05-16 15:01:42 -0300

    reorganizando

---
+++ ApocalipseCHI/src/engine/core/game/CanvasGame.java
@@ -0,0 +1,91 @@
+package engine.core.game;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import engine.core.MyCanvas;
+
+public class CanvasGame extends MyCanvas {
+
+	public static GameTesteState game;
+	
+	public CanvasGame() {
+		game = new GameTesteState();
+	}
+	
+	@Override
+	public void update(float diffTime) {
+		// TODO Auto-generated method stub
+		game.update(diffTime);
+	}
+
+	@Override
+	public void render(Graphics2D dbg) {
+		// TODO Auto-generated method stub
+		game.render(dbg);
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseWheelMoved(MouseWheelEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
