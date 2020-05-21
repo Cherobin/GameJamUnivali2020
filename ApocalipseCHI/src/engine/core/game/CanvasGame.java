c27e363fcd091389475d18704a9da779a9eba936
Cherobin <ricardo@cherobin.com.br>
2020-05-21 00:42:07 -0300

    arrumando gambiarra

--- ApocalipseCHI/src/engine/core/game/CanvasGame.java
+++ ApocalipseCHI/src/engine/core/game/CanvasGame.java
@@ -24,6 +24,7 @@ import engine.base.entities.GameObject;
 import engine.base.entities.GameState;
 import engine.core.GamePanel;
 import engine.core.MyCanvas;
+import engine.entities.Enemy;
 import engine.entities.Player;
 import engine.map.TileMap;
 import engine.map.TileMapLoader;
@@ -31,7 +32,7 @@ import engine.utils.Vector2D;
 import old.Constantes;
 
 public class CanvasGame extends MyCanvas implements GameState {
-	
+
 	private GameEntityBuilder seb;
 	private TileMapLoader tml;
 
@@ -40,79 +41,77 @@ public class CanvasGame extends MyCanvas implements GameState {
 	private org.mapeditor.core.Map map = null;
 
 	private int tamanhoSensor = 350;
-	
+
 	public static Map<String, GameObject> renderingStack;
-	public static List<GameObject> sortedEntities;
-	
+	public List<GameObject> sortedEntities;
+
 	BufferedImage roofImage;
-	
+
 	int[] roofData;
-	
 
 	public CanvasGame() {
-		
-		//tem que criar o player antes do tileMap por causa dos inimigos
-		//depois de tudo adiciona ele na lista
-		
-		player = new Player("Dennis", new Vector2D(800, 1000),32,32,0);
-		
-		roofImage = new BufferedImage(Constantes.telaW,Constantes.telaH,BufferedImage.TYPE_INT_ARGB);
-		roofData = ((DataBufferInt)roofImage.getRaster().getDataBuffer()).getData();
-		
+
+		// tem que criar o player antes do tileMap por causa dos inimigos
+		// depois de tudo adiciona ele na lista
+
+		player = new Player("Dennis", new Vector2D(800, 1000), 32, 32, 0);
+
+		roofImage = new BufferedImage(Constantes.telaW, Constantes.telaH, BufferedImage.TYPE_INT_ARGB);
+		roofData = ((DataBufferInt) roofImage.getRaster().getDataBuffer()).getData();
+
 		sortedEntities = new ArrayList<>();
 		renderingStack = new ConcurrentHashMap<>();
 		tileMap = new TileMap("tilemap");
-		
+
 		seb = new GameEntityBuilder();
 		tml = new TileMapLoader(seb);
-	  
- 
- 
-		map = tml.load(this, "res//maps//commercial01.tmx"); 
+
+		map = tml.load(this, "res//maps//commercial01.tmx");
 		tileMap.addMap(map);
-		map = tml.load(this, "res//maps//commercial01.tmx"); 
+		map = tml.load(this, "res//maps//commercial01.tmx");
 		tileMap.addMap(map);
-		map = tml.load(this, "res//maps//commercial01.tmx"); 
+		map = tml.load(this, "res//maps//commercial01.tmx");
 		tileMap.addMap(map);
-		
-		tileMap.setMap(); 
-	 
-		
-	    // adiciona o mapa para ele
+
+		tileMap.setMap();
+
+		// adiciona o mapa para ele
 		player.initializeComponents(tileMap);
 		addEntity(player);
-		   
-	 /*
-		Enemy enemy = new Enemy("Enemy",new Vector2D(24800, 24200),32,32,0,tileMap);
-		enemy.setTarget(player, 80, 300, 150, tileMap );
- 
-	 	addEntity(enemy);
-	 	
-		Enemy enemy2 = new Enemy("Enemy2",new Vector2D(24800, 24210),32,32,0,tileMap);
-		enemy2.setTarget(player, 60, 300, 150, tileMap );
- 
-	 	addEntity(enemy2);
-	 	
-		Enemy enemy3 = new Enemy("Enemy3",new Vector2D(24800, 24200),32,32,0,tileMap);
-		enemy3.setTarget(player, 50, 300, 150, tileMap );
- 
-	 	addEntity(enemy3);
-	 	
-		*/
-	 	
-		//TESTE INICIAL DE PLAY OGG
+
+		/*
+		 * Enemy enemy = new Enemy("Enemy",new Vector2D(24800, 24200),32,32,0,tileMap);
+		 * enemy.setTarget(player, 80, 300, 150, tileMap );
+		 * 
+		 * addEntity(enemy);
+		 * 
+		 * Enemy enemy2 = new Enemy("Enemy2",new Vector2D(24800,
+		 * 24210),32,32,0,tileMap); enemy2.setTarget(player, 60, 300, 150, tileMap );
+		 * 
+		 * addEntity(enemy2);
+		 * 
+		 * Enemy enemy3 = new Enemy("Enemy3",new Vector2D(24800,
+		 * 24200),32,32,0,tileMap); enemy3.setTarget(player, 50, 300, 150, tileMap );
+		 * 
+		 * addEntity(enemy3);
+		 * 
+		 */
+
+		// TESTE INICIAL DE PLAY OGG
 		OGG_Player musicplayer = new OGG_Player();
 		File ogg = new File("musica2_low.ogg");
 		musicplayer.ExamplePlayer(ogg);
-		//musicplayer.start();
-	 
-		  
+		// musicplayer.start();
+
 		// Attache target to enemy for EnemyBehavior component's sensors
 
-		/*
-		 * for (GameObject go : renderingStack.values()) { if (go instanceof Enemy) {
-		 * Enemy e = (Enemy) go; e.setTarget(player, 0.3f, 64, 256); } } addEntity(e)
-		 */
+		for (GameObject go : renderingStack.values()) {
+			if (go instanceof Enemy) {
+				Enemy e = (Enemy) go;
+				e.setTarget(player, 80, 300, 150, tileMap);
+				addEntity(e);
+			}
+		}
 
 	}
 
@@ -122,18 +121,18 @@ public class CanvasGame extends MyCanvas implements GameState {
 		// update map
 		if (tileMap != null)
 			tileMap.update(diffTime);
-		
+
 		for (int i = 0; i < sortedEntities.size(); i++) {
 			GameObject entity = sortedEntities.get(i);
-			if(!entity.isAlive()) {
-				sortedEntities.remove(i); 
+			if (!entity.isAlive()) {
+				sortedEntities.remove(i);
 				i--;
 				continue;
 			}
 			entity.update(diffTime);
 			if (entity.getComponents() != null && entity.getComponents().size() > 0) {
 				for (Component c : entity.getComponents().values()) {
-					//System.out.println(entity.getName() + " - " + c.getName());
+					// System.out.println(entity.getName() + " - " + c.getName());
 					c.update(entity, diffTime);
 				}
 			}
@@ -142,10 +141,10 @@ public class CanvasGame extends MyCanvas implements GameState {
 
 	@Override
 	public void render(Graphics2D dbg) {
-		//TODO Retirar depois
+		// TODO Retirar depois
 		dbg.setColor(Color.white);
 		dbg.fillRect(0, 0, Constantes.telaW, Constantes.telaH);
-		
+
 		// draw map
 		if (tileMap != null)
 			tileMap.render(dbg);
@@ -159,48 +158,45 @@ public class CanvasGame extends MyCanvas implements GameState {
 			}
 			entity.render(dbg);
 		}
-		
-		
+
 		// draw map
-		//System.out.println("underroof "+tileMap.isUnderRoof((int)player.position.x,(int)player.position.y));
-		if (tileMap != null && !tileMap.isUnderRoof((int)player.position.x,(int)player.position.y)) {
+		// System.out.println("underroof
+		// "+tileMap.isUnderRoof((int)player.position.x,(int)player.position.y));
+		if (tileMap != null && !tileMap.isUnderRoof((int) player.position.x, (int) player.position.y)) {
 			tileMap.renderRoof(dbg);
-		}else {
-			for(int i = 0; i < roofData.length;i++) {
+		} else {
+			for (int i = 0; i < roofData.length; i++) {
 				roofData[i] = 0;
 			}
-			Graphics2D graproof = (Graphics2D)roofImage.getGraphics();
-			graproof.clip(new Rectangle(0,0,Constantes.telaW,Constantes.telaH));
-			//System.out.println(" "+graproof.getClip());
+			Graphics2D graproof = (Graphics2D) roofImage.getGraphics();
+			graproof.clip(new Rectangle(0, 0, Constantes.telaW, Constantes.telaH));
+			// System.out.println(" "+graproof.getClip());
 			tileMap.renderRoof(graproof);
-			int cx = Constantes.telaW/2;
-			int cy = Constantes.telaH/2;
-			int distmax = 150*150;
-			for(int i = 0; i < Constantes.telaH;i++) {
-				for(int j = 0; j < Constantes.telaW;j++) {
+			int cx = Constantes.telaW / 2;
+			int cy = Constantes.telaH / 2;
+			int distmax = 150 * 150;
+			for (int i = 0; i < Constantes.telaH; i++) {
+				for (int j = 0; j < Constantes.telaW; j++) {
 					int dx = j - cx;
 					int dy = i - cy;
-					int dist = dx*dx+dy*dy;
-					if(dist<distmax) {
-						roofData[i*Constantes.telaW+j] = 0;
+					int dist = dx * dx + dy * dy;
+					if (dist < distmax) {
+						roofData[i * Constantes.telaW + j] = 0;
 					}
 				}
 			}
 			dbg.drawImage(roofImage, 0, 0, null);
 		}
-		
+
 		/*
-		//Aqui Liga e Desliga A Luz
-		tileMap.pontosdeluz[0][0] = 1;
-		tileMap.pontosdeluz[0][1] = Constantes.telaW/2;
-		tileMap.pontosdeluz[0][2] = Constantes.telaH/2;
-		tileMap.pontosdeluz[0][4] = tamanhoSensor;
-		tileMap.renderLigth(dbg);
-		*/
-		 
+		 * //Aqui Liga e Desliga A Luz tileMap.pontosdeluz[0][0] = 1;
+		 * tileMap.pontosdeluz[0][1] = Constantes.telaW/2; tileMap.pontosdeluz[0][2] =
+		 * Constantes.telaH/2; tileMap.pontosdeluz[0][4] = tamanhoSensor;
+		 * tileMap.renderLigth(dbg);
+		 */
 
 		dbg.setColor(Color.yellow);
-		//systemdata
+		// systemdata
 		drawSystemData(dbg);
 	}
 
@@ -212,7 +208,7 @@ public class CanvasGame extends MyCanvas implements GameState {
 		this.sortedEntities = sortedEntities;
 	}
 
-	public static void sortEntities() {
+	public void sortEntities() {
 
 		Collection<GameObject> ents = renderingStack.values();
 		sortedEntities.clear();
@@ -227,7 +223,7 @@ public class CanvasGame extends MyCanvas implements GameState {
 		});
 	}
 
-	public static void addEntity(GameObject entity) {
+	public void addEntity(GameObject entity) {
 		renderingStack.put(entity.getName(), entity);
 		sortEntities();
 	}
@@ -264,9 +260,9 @@ public class CanvasGame extends MyCanvas implements GameState {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		int keyCode = e.getKeyCode();
-		//player.direction = Direction.STOP;
+		// player.direction = Direction.STOP;
 		if (keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A) {
 			player.LEFT = false;
 		}
@@ -287,24 +283,24 @@ public class CanvasGame extends MyCanvas implements GameState {
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		player.mousePosition.set(e.getX(),e.getY());
+		player.mousePosition.set(e.getX(), e.getY());
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		player.mousePosition.set(e.getX(),e.getY());
+		player.mousePosition.set(e.getX(), e.getY());
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) { 
-		//fix estou sem mouse
+	public void mouseReleased(MouseEvent arg0) {
+		// fix estou sem mouse
 		player.FIRE = false;
 
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) { 
+	public void mousePressed(MouseEvent arg0) {
 		player.FIRE = true;
 	}
 
@@ -333,13 +329,12 @@ public class CanvasGame extends MyCanvas implements GameState {
 	}
 
 	@Override
-	public String getName() { 
+	public String getName() {
 		return this.getName();
 	}
 
-
 	@Override
-	public Map<String, GameObject> getEntities() { 
+	public Map<String, GameObject> getEntities() {
 		return renderingStack;
 	}
 
@@ -354,7 +349,7 @@ public class CanvasGame extends MyCanvas implements GameState {
 		sb.append("alloc:" + String.format("%06d", allocatedMemory / 1024) + "/");
 		sb.append("max:" + String.format("%06d", maxMemory / 1024) + "/");
 		sb.append("total:" + String.format("%06d", (freeMemory + (maxMemory - allocatedMemory)) / 1024));
-		sb.append(" FPS "+GamePanel.FPS);
+		sb.append(" FPS " + GamePanel.FPS);
 		String systemData = sb.toString();
 
 		String objects = String.format("nb-GE:%d", sortedEntities.size());
